dc62c57cab75555fbd523662717db8ca5dc2cf6a
Simon Brand <tartanllama@gmail.com>
2017-01-31 22:33:33 +0000

    Poll for file changes every 100 microseconds instead of every second

--- src/file_listener.cpp
+++ src/file_listener.cpp
@@ -23,7 +23,7 @@ namespace amer {
 		    }
 		} catch(...) {}
 	    }
-	    std::this_thread::sleep_for(std::chrono::seconds(1));
+	    std::this_thread::sleep_for(std::chrono::milliseconds(100));
 	}
     }
 }
