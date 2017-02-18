a014cb66eb05e3d6c775aae4f3b229b9d4fa2571
Simon Brand <tartanllama@gmail.com>
2017-02-18 21:47:16 +0000

    Clean up file listener code

--- src/file_listener.cpp
+++ src/file_listener.cpp
@@ -6,6 +6,8 @@
 
 namespace fs = std::experimental::filesystem;
 
+static constexpr auto sleep_time = std::chrono::milliseconds(100)
+
 namespace amer {
     void file_listener::run() {
 	m_listener_thread = std::thread{[this] { this->do_run(); }};
@@ -16,14 +18,19 @@ namespace amer {
 	    for (auto&& f : fs::recursive_directory_iterator(m_config.get_source_dir()/"content")) {
 		try {
 		    auto new_write_time = fs::last_write_time(f);
-		    if (!m_mod_times.count(f) || new_write_time != m_mod_times[f]) {
-			m_mod_times[f] = new_write_time;
+
+                    auto file_is_new = !m_mod_times.count(f);
+                    auto file_has_been_updated = new_write_time != m_mod_times[f];
+                    auto should_rerender = file_is_new || file_has_been_updated;
+
+		    if (should_rerender) {
+                        record_modification_time(f, new_write_time);
                         m_renderer.render_content(f);
 			m_server.refresh();
 		    }
 		} catch(...) {}
 	    }
-	    std::this_thread::sleep_for(std::chrono::milliseconds(100));
+	    std::this_thread::sleep_for(sleep_time);
 	}
     }
 }
